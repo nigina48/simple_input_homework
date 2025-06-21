efbcdc01cb48b5872becb9edc9e5b6ce8aa23ee8
Nigina <niginabultakova@gmail.com>
2025-06-21 20:02:02 +0500

    khusanovna

--- task7_remainder_of_division.py
+++ task7_remainder_of_division.py
@@ -2,7 +2,8 @@
 # Ask the user to enter two numbers and print the remainder when the first is divided by the second
 
 # Get input from user
-
+a = int(input(' a sonning kiriting: '))
+b = int(input(' b sonni kiriting'))
 # Calculate remainder
-
+print("sum", a / b)
 # Print result
