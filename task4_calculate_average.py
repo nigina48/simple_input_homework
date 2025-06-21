a3158d5a9acc38f63df101e6ddb5eb85dc7fbc9e
Nigina <niginabultakova@gmail.com>
2025-06-21 11:47:05 +0500

    nigina

--- task4_calculate_average.py
+++ task4_calculate_average.py
@@ -2,7 +2,8 @@
 # Ask the user to enter three numbers and print their average
 
 # Get input from user
-
+a = int(input( 'a sonning kiriting'))
+b = int(input( 'b sonni kiriting'))
 # Calculate average
-
+print("sum", a + b / 2)
 # Print result
