78a845a414127e1dd04d5340cd528537859e287a
Nigina <niginabultakova@gmail.com>
2025-06-21 11:39:48 +0500

    khusanovna

--- task2_area_of_rectangle.py
+++ task2_area_of_rectangle.py
@@ -2,7 +2,8 @@
 # Ask the user to enter the width and height of a rectangle and print its area
 
 # Get input from user
-
+a = int(input(' a sonning kiriting:' ))
+b = int (input( 'b sonni kiriting:'))
 # Calculate area
-
+print("sum" , a * b)
 # Print result
