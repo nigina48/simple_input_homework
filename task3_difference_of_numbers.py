e35d632c6d6c3db9410f55f2d7bb51445f82b8be
Nigina <niginabultakova@gmail.com>
2025-06-21 11:42:09 +0500

    nigina

--- task3_difference_of_numbers.py
+++ task3_difference_of_numbers.py
@@ -2,7 +2,8 @@
 # Ask the user to enter two numbers and print the difference (first minus second)
 
 # Get input from user
-
+a = int(input(' a sonning kiriting: '))
+b = int(input('b sonni kiriting: '))
 # Calculate difference
-
+print("sum", a-b)
 # Print result
