57d40723f8817a335e6422a98dc710a78a2f69be
Nigina <niginabultakova@gmail.com>
2025-06-21 11:59:51 +0500

    khusanovna

--- task5_celsius_to_fahrenheit.py
+++ task5_celsius_to_fahrenheit.py
@@ -2,7 +2,9 @@
 # Ask the user to enter a temperature in Celsius and print it in Fahrenheit
 
 # Get input from user
-
+a = int(input(' a sonning kiriting'))
+b = int(input(' b sonni kiriting'))
+c = int(input(' c sonni kiriting'))
 # Convert to Fahrenheit (F = C × 1.8 + 32)
-
+print("sum", c * a + b)
 # Print result
