176e157525b0948b2b2a95bd06de92ac37c64502
Nigina <niginabultakova@gmail.com>
2025-06-21 11:35:37 +0500

    nigina

--- task1_add_two_numbers.py
+++ task1_add_two_numbers.py
@@ -2,7 +2,9 @@
 # Ask the user to enter two numbers and print their sum
 
 # Get input from user
+a = int( input ( 'a sonning kiriting: '))
+b = int ( input ( ' b sonni kiriting '))
 
 # Calculate sum
-
+print("sum" , a + b)
 # Print result
