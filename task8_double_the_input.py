519cbd4f32aa42e530b59aa7675a416886876b8b
Nigina <niginabultakova@gmail.com>
2025-06-23 20:45:33 +0500

    khusanovna

--- task8_double_the_input.py
+++ task8_double_the_input.py
@@ -2,7 +2,8 @@
 # Ask the user to enter a number and print twice its value
 
 # Get input from user
+a = int(input( 'a sonning kiriting: '))
 
 # Calculate double
-
+print("sum", a * 2)
 # Print result
