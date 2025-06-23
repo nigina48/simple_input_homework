0dd9f89786fdf7e2313a5ef238cdbb5f8aacbaae
Nigina <niginabultakova@gmail.com>
2025-06-23 20:34:17 +0500

    khusanovna

--- task10_multiply_three_numbers.py
+++ task10_multiply_three_numbers.py
@@ -2,7 +2,9 @@
 # Ask the user to enter three numbers and print their product
 
 # Get input from user
-
+a = int(input( 'a sonning kiriting: '))
+b = int(input( 'b sonni kiriting: '))
+c = int(input( 'c sonni kiriting '))
 # Calculate product
-
+print("sum", a * b * c )
 # Print result
