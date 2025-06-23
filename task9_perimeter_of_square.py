dbdbe58262cacd58714c9d810e1300332e5b880d
Nigina <niginabultakova@gmail.com>
2025-06-23 09:01:10 +0500

    nigina

--- task9_perimeter_of_square.py
+++ task9_perimeter_of_square.py
@@ -2,7 +2,8 @@
 # Ask the user to enter the length of a square's side and print its perimeter
 
 # Get input from user
-
+a = int(input( 'a sonning kiriting: '))
+b = int(input( 'b sonni kiriting: '))
 # Calculate perimeter
-
+print("sum", 2* (a + b))
 # Print result
